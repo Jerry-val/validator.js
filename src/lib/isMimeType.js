4e707ead268a35ee37e058737943d31ed37a643b
Guo Jiarui <799423879@qq.com>
2026-09-07 12:10:08 +0800

    fix(isMimeType): allow underscores in parameterized MIME types

--- src/lib/isMimeType.js
+++ src/lib/isMimeType.js
@@ -29,10 +29,10 @@ import assertString from './util/assertString';
 const mimeTypeSimple = /^(application|audio|font|image|message|model|multipart|text|video)\/[a-zA-Z0-9\.\-\+_]{1,100}$/i; // eslint-disable-line max-len
 
 // Handle "charset" in "text/*"
-const mimeTypeText = /^text\/[a-zA-Z0-9\.\-\+]{1,100};\s?charset=("[a-zA-Z0-9\.\-\+\s]{0,70}"|[a-zA-Z0-9\.\-\+]{0,70})(\s?\([a-zA-Z0-9\.\-\+\s]{1,20}\))?$/i; // eslint-disable-line max-len
+const mimeTypeText = /^text\/[a-zA-Z0-9\.\-\+_]{1,100};\s?charset=("[a-zA-Z0-9\.\-\+_\s]{0,70}"|[a-zA-Z0-9\.\-\+_]{0,70})(\s?\([a-zA-Z0-9\.\-\+\s]{1,20}\))?$/i; // eslint-disable-line max-len
 
 // Handle "boundary" in "multipart/*"
-const mimeTypeMultipart = /^multipart\/[a-zA-Z0-9\.\-\+]{1,100}(;\s?(boundary|charset)=("[a-zA-Z0-9\.\-\+\s]{0,70}"|[a-zA-Z0-9\.\-\+]{0,70})(\s?\([a-zA-Z0-9\.\-\+\s]{1,20}\))?){0,2}$/i; // eslint-disable-line max-len
+const mimeTypeMultipart = /^multipart\/[a-zA-Z0-9\.\-\+_]{1,100}(;\s?(boundary|charset)=("[a-zA-Z0-9\.\-\+_\s]{0,70}"|[a-zA-Z0-9\.\-\+_]{0,70})(\s?\([a-zA-Z0-9\.\-\+\s]{1,20}\))?){0,2}$/i; // eslint-disable-line max-len
 
 export default function isMimeType(str) {
   assertString(str);
